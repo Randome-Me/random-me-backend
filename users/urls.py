4d41d1b32a2090fe411827b7b676438fb7d56b44
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-30 02:39:42 +0700

    Create auth/logout endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from users.views import RegisterView
 from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
-    path('me/', UserView.as_view())
+    path('me/', UserView.as_view()),
+    path('logout/', LogoutView.as_view())
 ]
