13404c842b5adf0e63981985e1b190e4e33afcb5
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-30 02:24:04 +0700

    Create auth/register endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from rest_framework.parsers import JSONParser
+from django.contrib.auth.models import User
+from rest_framework import exceptions
+import jwt, datetime
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        password = request.data['password']
+        confirmPassword = request.data['confirmPassword']
+        
+        if  password != confirmPassword:
+            return Response({
+                'message': 'Passwords do not match.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()
+        return Response(serializer.data)
