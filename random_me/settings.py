2dcd5e60ef3a6e8c339edf15408a4fb480b7b997
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-30 01:30:33 +0700

    Add 'users' app

--- random_me/settings.py
+++ random_me/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
