e650a950ae7193f06d64cdafc6e730859827a8d2
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-31 01:51:27 +0700

    Change username and password in utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def get_db_handle(db_name, host, port, username, password):
 
  client = MongoClient(host=os.environ.get('DATABASE_HOST'),
                       port=int(port),
-                      username=username,
-                      password=password
+                      username=os.environ.get('DATABASE_USERNAME'),
+                      password=os.environ.get('DATABASE_PASSWORD')
                      )
  db_handle = client['db_name']
  return db_handle, client
