103c49f33ae97142280c9cea47b6f77523628241
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-30 15:53:13 +0700

    Edit utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 def get_db_handle(db_name, host, port, username, password):
 
  client = MongoClient(host=host,
-                      port=int(port),
+                      port=int(port) | os.environ.get('DATABASE_PORT'),
                       username=username,
                       password=password
                      )
