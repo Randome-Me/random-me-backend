4f130827306887ff1ba85fc26e1a2a6f56e6da46
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2021-12-31 03:09:07 +0700

    Remove file in .gitignore

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn random_me.wsgi
--- topics/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import models
-from rest_framework import serializers
-from .models import AppUser
-
-class AppUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AppUser
-        fields = ['username', 'selectedTopicId', 'topic']
