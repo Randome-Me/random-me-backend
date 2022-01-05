978180f94ce7fd4efff383d3dc0776a3c2f8df9f
nutnicha-p <69999838+nutnicha-p@users.noreply.github.com>
2022-01-05 21:30:24 +0700

    Create Reset{asswordToken

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models.fields import CharField, IntegerField
+from djongo import models
+import uuid
+from datetime import datetime, timedelta
 
 # Create your models here.
+class ResetPasswordToken(models.Model):
+    userId = models.IntegerField()
+    uuidToken = models.CharField(max_length=36, default=uuid.uuid4(), unique=True, primary_key=True)
+    expDate = models.DateTimeField(default=datetime.now()+timedelta(hours=12))
